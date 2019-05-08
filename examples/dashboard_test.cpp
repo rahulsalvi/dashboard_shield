ced40adcd5b373a157b53ab32283108967aca592
Rahul Salvi <rsalvi006@me.com>
2019-05-08 15:44:48 -0700

    example sketch - to be tested

--- examples/dashboard_test.cpp
+++ examples/dashboard_test.cpp
@@ -4,14 +4,13 @@
 using dashboard_shield::dashboard_t;
 
 static dashboard_t dashboard;
+static int         counter;
 
 void setup() {
-    dashboard_shield::begin();
-
-    dashboard.rgb_leds[0] = DS_RGB_RED;
-    dashboard.rgb_leds[1] = DS_RGB_GRN;
-    dashboard.rgb_leds[2] = DS_RGB_BLU;
-    dashboard.rgb_leds[3] = DS_RGB_RED;
+    dashboard.rgb_leds[0] = DS_RGB_OFF;
+    dashboard.rgb_leds[1] = DS_RGB_RED;
+    dashboard.rgb_leds[2] = DS_RGB_GRN;
+    dashboard.rgb_leds[3] = DS_RGB_BLU;
 
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 32; j++) {
@@ -21,12 +20,14 @@ void setup() {
             dashboard.pixel_channels[i].pixels[j].wht = 0;
         }
     }
-}
 
-static int counter = 0;
+    counter = 0;
+    dashboard_shield::begin();
+}
 
 void loop() {
     for (int i = 0; i < 4; i++) {
+        dashboard.rgb_leds[i] = (counter + i) % 4;
         for (int j = 0; j < 32; j++) {
             dashboard.pixel_channels[i].pixels[j].red = (counter == 0) * 255;
             dashboard.pixel_channels[i].pixels[j].grn = (counter == 1) * 255;
@@ -35,7 +36,7 @@ void loop() {
         }
     }
     dashboard_shield::update(dashboard);
-    counter = (counter + 1 > 3) ? (0) : (counter + 1);
+    counter = (counter + 1) % 4;
     delay(1000);
 }
 
